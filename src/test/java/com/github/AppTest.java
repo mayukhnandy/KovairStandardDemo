1fb86e0c9dd6ba85fbc010c69b0bd765acf291af
mayukhnandy <sd191@demo1.sr.kovair.com>
2019-03-07 21:33:12 -0800

    ok

--- src/test/java/com/github/AppTest.java
+++ src/test/java/com/github/AppTest.java
@@ -28,13 +28,13 @@ public class AppTest
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+    public static TestSuite suite()
     {
         return new TestSuite( AppTest.class );
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigorous Test :-)
      */
     public void testApp()
     {
