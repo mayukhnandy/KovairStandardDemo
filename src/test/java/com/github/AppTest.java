69a903540ca55aef8882503ae52554e2327ee475
mayukhnandy <sd191@demo1.sr.kovair.com>
2019-03-11 00:19:14 -0700

    37

--- src/test/java/com/github/AppTest.java
+++ src/test/java/com/github/AppTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 //Pending comments
 
 //Some comments on the master branch
-//Added some comments on the branch for the egit demo
+//Added some comments on the branch for the egit demol
 
 /**
  * Unit test for simple App.
